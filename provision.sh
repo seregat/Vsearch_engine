fa0dd746f67ae68ef9527a7c36219602645f3d20
Sergey Titievsky <titievsky.sergey@gmail.com>
2016-07-09 01:25:21 +0300

    Revert "project structure refactoring"
    This reverts commit 701f330ff1e770fda12ed271e64d6bbe2efb9afa.

--- provision.sh
+++ provision.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env bash
 sudo -i
+
 sudo yum -y update
 sudo yum -y install python libxml2 libxml2-devel python-setuptools zlib-devel wget openssl-devel pcre pcre-devel sudo gcc make autoconf automake kernel-devel
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install epel-release
 sudo yum install python-pip python-devel nginx gcc
 sudo yum install -y vim-X11 vim-common vim-enhanced vim-minimal
-sudo yum -y install net-tools strace socat
+
+
+
+
+#yum clean all
+#yum makecache
+
+
 sudo yum install -y htop
 sudo yum install -y vim
 sudo yum install -y nginx
@@ -20,41 +27,30 @@ make
 make install
 ln -s /usr/local/bin/pip3.4 /usr/bin/pip3.4
 ln -s /usr/local/bin/pyvenv-3.4 /usr/bin/virtualenv3.4
-
-
+cd /usr/share
 mkdir /usr/share/search_engine_env_serve
 cd /usr/share/search_engine_env_serve
+#rm -rf /usr/share/search_engine_env_serve
+#cp -r /usr/share/search_engine_env /usr/share/search_engine_env_serve
+#cd /usr/share/search_engine_env_serve
+#find /usr/share/search_engine_env_serve -type d -print0 | xargs -0 chmod 770 
+#find /usr/share/search_engine_env_serve -type f -print0 | xargs -0 chmod 660
+#chown -R  vagrant:vagrant  /usr/share/search_engine_env_serve
 virtualenv3.4 env
 source env/bin/activate
+#python3.4 setup.py install
 pip3.4 install --upgrade pip
-
-
-#START INSTALL APPLICATION
-git init
-git remote add -f origin https://github.com/seregat/Vsearch_engine.git
-git config core.sparseCheckout true
-echo "search_engine_app" >> .git/info/sparse-checkout
-git pull origin master
-python3.4 search_engine_env/setup.py install
-#END INSTALL APPLICATION
+#pip3.4 install uwsgi
+#python3.4 setup.py register sdist upload
+pip3.4 install search_engine_app
+#this service defined in search_engine_env_serve/setup.py
 
 
 
-#this option
-#pip3.4 install search_engine_app
-deactivate
 
-pip3.4 install --upgrade pip
-pip3.4 install uwsgi
-ln -s /usr/share/search_engine_env_serve/env/lib/python3.4/site-packages/search_engine_app  usr/share/search_engine_env_serve/app
-ln -s /usr/share/search_engine_env_serve/env/lib/python3.4/site-packages  usr/share/search_engine_env_serve/src
-mkdir /usr/share/search_engine_env_serve/etc
-
-
-
-cat > /etc/systemd/system/search_engine_uwsgi.service <<EOF
+cat > /etc/systemd/system/search_engine.service <<EOF
 [Unit]
-Description=Search Engine uwsgi Service
+Description=Flask Search Engine Service
 After=network.target
 
 [Service]
@@ -62,111 +58,17 @@ User=root
 Group=root
 WorkingDirectory=/usr/share/search_engine_env_serve
 Environment="PATH=/usr/share/search_engine_env_serve/env/bin"
-ExecStartPre=/bin/sh -c "systemctl start nginx"
-ExecStart=/bin/sh -c "source /usr/share/search_engine_env_serve/env/bin/activate"
-ExecStartPost=/bin/sh -c "uwsgi --ini /usr/share/search_engine_env_serve/etc/uwsgi.ini"
+ExecStart=/usr/share/search_engine_env_serve/env/bin/serve
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
 
-cat > /etc/systemd/system/search_engine_serve.service <<EOF
-[Unit]
-Description=Search Engine Service
-After=network.target
+sudo systemctl start search_engine
+sudo systemctl enable search_engine
 
-[Service]
-Type=forking
-User=root
-Group=root
-ExecStart=/bin/sh -c "service nginx start&;service search_engine_uwsgi start&"
-ExecStop=/bin/sh -c "service nginx stop;service search_engine_uwsgi stop"
-[Install]
-WantedBy=multi-user.target
-EOF
-
-
-cat > /usr/share/search_engine_env_serve/etc/uwsgi.ini <<EOF
-[uwsgi]
-wsgi-file = /usr/share/search_engine_env_serve/app/flask_app.py
-home =  /usr/share/search_engine_env_serve/env
-pythonpath =  /usr/share/search_engine_env_serve/src
-virtualenv = /usr/share/search_engine_env_serve/env
-callable = app
-master = true
-socket = localhost:9000
-vacuum = true
-processes = 2
-uid = vagrant
-gid = vagrant
-pidfile = /tmp/bc_local.pid
-no-site = True
-enable-threads = True
-py-autoreload = 1
-disable-logging = 1
-die-on-term = true
-EOF
-
-cat >  /etc/nginx/nginx.conf<<EOF
-user vagrant;
-worker_processes auto;
-pid /run/nginx.pid;
-error_log /var/log/nginx/error.log;
-
-events {
-    worker_connections 1024;
-}
-
-http {
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile            on;
-    tcp_nopush          on;
-    tcp_nodelay         on;
-    keepalive_timeout   65;
-    types_hash_max_size 2048;
-
-    include             /etc/nginx/mime.types;
-    default_type        application/octet-stream;
-    include /etc/nginx/conf.d/*.conf;
-    server {
-        listen       80 default_server;
-        listen       [::]:80 default_server;
-        server_name  search_engine_app;
-        include /etc/nginx/default.d/*.conf;
-        location / {
-                include         uwsgi_params;
-                uwsgi_pass      localhost:9000;
-        }
-        location /static {
-            root  /usr/share/search_engine_env_serve/app/;
-        }
-
-        root         /usr/share/search_engine_env_serve/app/static;
-
-        error_page 404 /404.html;
-            location = /usr/share/search_engine_env_serve/app/templates/404.html{
-        }
-
-        error_page 500 502 503 504 /50x.html;
-            location = /50x.html {
-        }
-    }
-}
-
-EOF
-
-
-
-
-
-sudo systemctl start search_engine_serve
-sudo systemctl enable search_engine_serve
-
+service search_engine status
 
 
 
